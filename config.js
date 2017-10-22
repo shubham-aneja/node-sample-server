56a5c2df6594d2dcbd6792921d78411bb75cf6d0
shubham-aneja <shubham.aneja@3pillarglobal.com>
2017-10-22 20:14:52 +0530

    sample server setup

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    serverPort: 5000,
-    mongoUrl: 'localhost:27017',
-    dbName: 'pocket-manager',
-    usersTable: 'users',
-    tokenTable: 'Connections',
-    transactionsTable: 'transactions',
-    accountsTable: 'accounts'
-};
